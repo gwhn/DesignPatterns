d035d1804186a08404079ce2887d8d7cbc4e66b6
Guy Newman <gwhn@weblitz.co.uk>
2014-01-29 19:56:10 +0000

    Added Prototype pattern

---
+++ DesignPatterns/Prototype.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatterns
+{
+    public class Prototype : IPrototype<Prototype>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Prototype Clone()
+        {
+            return (Prototype) MemberwiseClone();
+        }
+    }
+
+    public interface IPrototype<out T>
+    {
+        T Clone();
+    }
+
+    [TestClass]
+    public class PrototypeTests
+    {
+        [TestMethod]
+        public void Clone()
+        {
+            var type = new Prototype();
+            var copy = type.Clone();
+            Assert.IsTrue(type.Id == copy.Id);
+            Assert.IsTrue(type.Name == copy.Name);
+        }
+    }
+}
