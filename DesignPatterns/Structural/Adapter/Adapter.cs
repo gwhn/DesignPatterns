871a66094af80e3c751342d0a12420d52056fe3a
Guy Newman <gwhn@weblitz.co.uk>
2014-01-30 12:56:35 +0000

    Added Adapter pattern

---
+++ DesignPatterns/Structural/Adapter/Adapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatterns.Structural.Adapter
+{
+    /// <summary>
+    /// Convert the interface of a class into another interface clients expect. 
+    /// Adapter lets classes work together that couldn't otherwise because of incompatible interfaces. 
+    /// </summary>
+    [TestClass]
+    public class AdapterTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var target = new Adapter();
+            target.Request();
+        }
+    }
+
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Target.Request");
+        }
+    }
+
+    public class Adapter : Target
+    {
+        private readonly Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    internal class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Adaptee.SpecificRequest");
+        }
+    }
+}
