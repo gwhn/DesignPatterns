784f1f4b0f009d7b2a27ff3e643e708d8a2406ce
Guy Newman <gwhn@weblitz.co.uk>
2014-01-31 11:47:47 +0000

    Added TemplateMethod pattern

---
+++ DesignPatterns/Behavioral/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.TemplateMethod
+{
+    /// <summary>
+    /// Define the skeleton of an algorithm in an operation, deferring some steps to subclasses. 
+    /// Template Method lets subclasses redefine certain steps of an algorithm 
+    /// without changing the algorithm's structure.
+    /// </summary>
+    [TestClass]
+    public class TemplateMethodTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var typeA = new ConcreteTypeA();
+            typeA.TemplateMethod();
+            var typeB = new ConcreteTypeB();
+            typeB.TemplateMethod();
+        }
+    }
+
+    public abstract class AbstractType
+    {
+        public abstract void PrimitiveMethodA();
+        public abstract void PrimitiveMethodB();
+
+        public void TemplateMethod()
+        {
+            PrimitiveMethodA();
+            PrimitiveMethodB();
+        }
+    }
+
+    public class ConcreteTypeA : AbstractType
+    {
+        public override void PrimitiveMethodA()
+        {
+            Console.WriteLine("ConcreteTypeA.PrimitiveMethodA");
+        }
+
+        public override void PrimitiveMethodB()
+        {
+            Console.WriteLine("ConcreteTypeA.PrimitiveMethodB");
+        }
+    }
+
+    public class ConcreteTypeB : AbstractType
+    {
+        public override void PrimitiveMethodA()
+        {
+            Console.WriteLine("ConcreteTypeB.PrimitiveMethodA");
+        }
+
+        public override void PrimitiveMethodB()
+        {
+            Console.WriteLine("ConcreteTypeB.PrimitiveMethodB");
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Visitor/Visitor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Visitor
+{
+    /// <summary>
+    /// Represent an operation to be performed on the elements of an object structure. 
+    /// Visitor lets you define a new operation without changing the classes of the 
+    /// elements on which it operates. 
+    /// </summary>
+    [TestClass]
+    public class VisitorTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
