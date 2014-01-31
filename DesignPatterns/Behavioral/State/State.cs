5012436e7685119427ba66b69ef2c924067bfa67
Guy Newman <gwhn@weblitz.co.uk>
2014-01-31 11:37:15 +0000

    Added State and Strategy patterns

---
+++ DesignPatterns/Behavioral/State/State.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatterns.Behavioral.State
+{
+    /// <summary>
+    /// Allow an object to alter its behavior when its internal state changes. 
+    /// The object will appear to change its class.
+    /// </summary>
+    [TestClass]
+    public class StateTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+        }
+    }
+
+    public abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    public class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+        }
+    }
+
+    public class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+    public class Context
+    {
+        private State _state;
+
+        public Context(State state)
+        {
+            State = state;
+        }
+
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State: " + _state.GetType().Name);
+            }
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+}
---
+++ DesignPatterns/Behavioral/Strategy/Strategy.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+    /// <summary>
+    /// Define a family of algorithms, encapsulate each one, and make them interchangeable. 
+    /// Strategy lets the algorithm vary independently from clients that use it.
+    /// </summary>
+    [TestClass]
+    public class StrategyTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var context = new Context(new ConcreteStrategyA());
+            context.Operation();
+            context = new Context(new ConcreteStrategyB());
+            context.Operation();
+            context = new Context(new ConcreteStrategyC());
+            context.Operation();
+        }
+    }
+
+    public class Context
+    {
+        private Strategy _strategy;
+
+        public Context(Strategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void Operation()
+        {
+            _strategy.Operation();
+        }
+    }
+
+    public abstract class Strategy
+    {
+        public abstract void Operation();
+    }
+
+    public class ConcreteStrategyA : Strategy
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteStrategyA.Operation");
+        }
+    }
+
+    public class ConcreteStrategyB : Strategy
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteStrategyB.Operation");
+        }
+    }
+
+    public class ConcreteStrategyC : Strategy
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteStrategyC.Operation");
+        }
+    }
+}
